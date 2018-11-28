561f347976c5a52a422716f76e6eac64abb8a66e
Hongjun He <anthonyho02015@gmail.com>
2018-11-28 02:03:39 +0000

    added radio button, does not work

--- js/script.js
+++ js/script.js
@@ -3,12 +3,60 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
+        console.log(name);
+        console.log($("question4").prop('checked'));
+
+        
+        
+        // var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+        // var q1Result = $("#question1").val();
+        // var q2Result = $("#question2").val();
+        // var q3Result = $("#question3").val();
         var totalScore;
+    
+    //questions    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     });
 
 });
