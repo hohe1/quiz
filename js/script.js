4fe830d311197d04c1d92e75a51cb4662f76680a
Hongjun He <anthonyho02015@gmail.com>
2019-01-08 07:04:18 +0000

    added results.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 //$(document).ready(function(){
     var options = [`Some random guy who hates alcohol`,`Jay Gatsby`,`nick carraway`,
-    `Daisy Buchanan`,`Tom Buchanan","Jordan Baker`,`dog`,`Myrtle Wilson`,
+    `Daisy Buchanan`,`Tom Buchanan`,`Jordan Baker`,`dog`,`Myrtle Wilson`,
     `George Wilson`];
 
     var q2A;
@@ -14,6 +14,8 @@
     var q7A;
     
     $("button").click(function() {
+    
+        
         var name = $("#name").val();
         question1();
         question2();
@@ -22,7 +24,7 @@
         question5();
         question7();
         console.log(name+q2A+q3A+q4A+q5A+q6A+q7A);
-        
+        console.log(options);
         
     });
         
@@ -51,8 +53,19 @@ function question2(){
     if(q2.toLowerCase().trim() === "alcohol"){
         q2A = true;
         
+        removes("Jay Gatsby");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jordan Baker");
+        removes("Myrtle Wilson");
+        removes("George Wilson");
+        
     }else if(q2.trim() !== ""){
         q2A = false;
+        
+        removes("Some random guy who hates alcohol");
+        
     }else{
         //literally nothing
     }
@@ -63,8 +76,21 @@ function question3(){
     var q3 = $("#question3").val();
     if(q3.toLowerCase().trim() === "green light"){
         q3A = true;
+        
+        removes("Some random guy who hates alcohol");
+        removes("dog");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jordan Baker");
+        removes("Myrtle Wilson");
+        removes("George Wilson");
+        
     }else if(q3.toLowerCase().trim() !== ""){
         q3A = false;
+        
+        //gatsby remove?
+        
     }else{
         //literally nothing
     }
@@ -75,8 +101,20 @@ function question4(){
     var q4 = $("input[name='c']:checked").val();      
         if(q4 === "yes"){
             q4A = true;
+            
+        removes("Some random guy who hates alcohol");
+        removes("Jay Gatsby");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jordan Baker");
+
         }else if(q4 === "no"){
-            q4A = false;   
+            q4A = false;
+            
+        removes("Myrtle Wilson");
+        removes("George Wilson");
+            
         }else{
         //nothing
         }
@@ -87,8 +125,19 @@ function question5(){
     var q5 = $("input[name = 'b']:checked").val();
         if(q5 ==="yes" || q5 ==="someTimes"){
             q5A = true;
+            
+        removes("George Wilson");
+        
+
         }else if(q5 ==="no"){
             q5A = false;
+            
+        removes("Myrtle Wilson");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jordan Baker");
+
         }else{/*nothing at all*/}
     }
     
@@ -97,8 +146,19 @@ function question6(){
     var q6 = $("input[name = 'a']:checked").val();
         if(q6 ==="yes"){
             q6A = true;
+            
+        removes("Myrtle Wilson");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jay Gatsby");
+
         }else if(q6 ==="no"){
             q6A = false;
+            
+        removes("Daisy Buchanan");
+        removes("Jordan Baker");
+            
         }else{/*nothing at all*/} 
 }
 //  question 7 range
@@ -106,9 +166,23 @@ function question7(){
     var q7 = $("input[name = 'd']:checked").val();
         if(q7 ==="yes"){
             q7A = true;
+            
+        removes("Jay Gatsby");
+        removes("nick carraway");
+        removes("Daisy Buchanan");
+        removes("Tom Buchanan");
+        removes("Jordan Baker");
+        removes("Myrtle Wilson");
+        removes("George Wilson");
+        removes("Some random guy who hates alcohol");
+    
+            
             console.log("you're a dog.");
         }else if(q7 ==="no"){
             q7A = false;
+            
+            removes("dog")
+            
         }else{/*nothing at all*/} 
 }
     
@@ -117,7 +191,7 @@ function question7(){
 function removes(Cname){
     if(options.indexOf(Cname) !== -1){
         options.splice(options.indexOf(Cname), 1);
-        console.log("beep");
+        console.log("removed "+Cname);
     }
     
     
